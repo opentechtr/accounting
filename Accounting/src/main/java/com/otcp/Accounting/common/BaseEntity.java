38291a2e20698798727b01c216b3b9f62cd806dd
Ali <ali.simsek035@gmail.com>
2025-02-20 01:45:14 +0300

    Added baseEntity

--- Accounting/src/main/java/com/otcp/Accounting/common/BaseEntity.java
+++ Accounting/src/main/java/com/otcp/Accounting/common/BaseEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @Id
     @Column(nullable = false, updatable = false)
